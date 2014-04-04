96b44757d687b1dae4ac9dcecf4340dbccce403c
Helier Cortez <hdnymib@gmail.com>
2014-04-04 13:14:13 -0400

    fixed sum module sell

--- venta/protected/views/distribuidora/_newRowDetalleVenta.php
+++ venta/protected/views/distribuidora/_newRowDetalleVenta.php
@@ -14,32 +14,35 @@
 <td class="col-sm-1">
 	<?php echo CHtml::activeTextField(Almacen::model(),"[$index]stockUnidad",array('class'=>'form-control input-sm','id'=>'stockUnidad_'.$index)); ?>
 	<?php echo CHtml::error(Almacen::model(),"[$index]stockUnidad"); ?>
+	<?php echo CHtml::hiddenField("unidad".$index,$almacen->Producto->costoCFUnidad)?>
 </td>
 
 <td class="col-sm-1">
 	<?php echo CHtml::activeTextField(Almacen::model(),"[$index]stockPaquete",array('class'=>'form-control input-sm','id'=>'stockPaquete_'.$index)); ?>
 	<?php echo CHtml::error(Almacen::model(),"[$index]stockPaquete"); ?>
+	<?php echo CHtml::hiddenField("paquete".$index,$almacen->Producto->costoCF)?>
 </td>
 <td class="col-sm-1">
 	<?php echo CHtml::activeTextField($detalle,"[$index]costoTotal",array('class'=>'form-control input-sm','disabled'=>true,'id'=>'costoTotal_'.$index)); ?>
 </td>
 
-<script type="text/javascript">
+<?php  echo 
+"
+<script>
 	function suma(a,b)
 	{
-		var c;
-		a=a*<?php echo $almacen->Producto->costoCFUnidad ?>;
-		b=b*<?php echo $almacen->Producto->costoCF ?>;
 		return (a+b);
 	}
 		
-	$('#stockUnidad_<?php echo $index ?>').blur(function(e){ 
-	    $('#costoTotal_<?php echo $index ?>').val(suma($('#stockUnidad_<?php echo $index ?>').val(),$('#stockPaquete_<?php echo $index ?>').val()));
+	$('#stockUnidad_". $index ."').blur(function(e){ 
+	    $('#costoTotal_". $index ."').val(suma($('#stockUnidad_".  $index ."').val()*$('#unidad". $index ."').val(),$('#stockPaquete_". $index ."').val()*$('#paquete". $index ."').val()));
 		return true;
 	});
 	
-	$('#stockPaquete_<?php echo $index ?>').blur(function(e){ 
-	    $('#costoTotal_<?php echo $index ?>').val(suma($('#stockUnidad_<?php echo $index ?>').val(),$('#stockPaquete_<?php echo $index ?>').val()));
+	$('#stockPaquete_". $index ."').blur(function(e){ 
+	    $('#costoTotal_". $index ."').val(suma($('#stockUnidad_". $index ."').val()*$('#unidad". $index ."').val(),$('#stockPaquete_". $index ."').val()*$('#paquete". $index ."').val()));
 	  	return true;
 	});
 </script>
+
+";?>
